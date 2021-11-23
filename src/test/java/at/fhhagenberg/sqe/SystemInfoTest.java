a118ab464a6ad77de89b654d89f47df0d28c4949
Mario Winterer <mario.winterer@scch.at>
2021-11-23 17:43:27 +0000

    updgrade to java 17 LTS

--- src/test/java/at/fhhagenberg/sqe/SystemInfoTest.java
+++ src/test/java/at/fhhagenberg/sqe/SystemInfoTest.java
@@ -7,11 +7,11 @@ import org.junit.jupiter.api.Test;
 public class SystemInfoTest {
     @Test
     public void testJavaVersion() {
-        assertEquals("13", SystemInfo.javaVersion());
+        assertEquals("17.0.1", SystemInfo.javaVersion());
     }
 
     @Test
     public void testJavafxVersion() {
-        assertEquals("13", SystemInfo.javafxVersion());
+        assertEquals("17.0.1", SystemInfo.javafxVersion());
     }
 }
